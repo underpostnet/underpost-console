b39629ab796bbf8fbdcbd9396b4cf8af71a8f1da
fcoverdugoa <fcoverdugoa@underpost.net>
2021-07-03 10:45:47 -0400

    update

--- api/koyn/bin/undermine.js
+++ api/koyn/bin/undermine.js
@@ -3,84 +3,28 @@
 //  Developed By Francisco Verdugo <fcoverdugoa@underpost.net>
 //  https://github.com/underpostnet/underpost-network
 
-// https://cryptojs.gitbook.io/docs/
-// const  = require('crypto-js/sha256');
+// KOYN BLOCKCHAIN SYSTEM
 
 import { BlockChain } from "../class/blockChain.js";
-import { RestService } from "../../../../../src/rest/class/RestService.js";
-import { Util } from "../../../../../src/util/class/Util.js";
-import fs from "fs";
 
-var KOYN = new BlockChain();
-var pathData;
-
-var storage = {};
-async function addBlock(storage, resolve){
-  KOYN.addBlock({
-    data: storage,
-    dataTransaction: {content: "test"},
-    rewardAddress: "XXXXXXXXXXXXXXX"
-  });
-  storage = {};
-  resolve();
-};
-
-function generateBlock() {
-  return new Promise(resolve => {
-
-    // pathData = 'http://localhost:3001/koyn';
-    pathData = 'https://www.cyberiaonline.com/koyn';
-    new RestService().getContent(pathData , storage,
-      (data, storage)=>{
-        console.log('restService success -> '+pathData);
-        storage.cyberiaonline = data;
-
-              // pathData = 'http://localhost:3001/cards';
-              pathData = 'https://www.cyberiaonline.com/cards';
-              new RestService().getContent(pathData , storage,
-                (data, storage)=>{
-                  console.log('restService success -> '+pathData);
-                  storage.underPostCardGame = data;
-
-                            addBlock(storage, resolve);
-
-                },
-                (error)=>{
-                  console.log('restService error -> '+pathData);
-                  console.log(error);
-                }
-              );
-
-      },
-      (error)=>{
-        console.log('restService error -> '+pathData);
-        console.log(error);
-      }
-    );
-
-
-  });
-}
-
-async function mainProcess() {
-  // https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Statements/async_function
-  await generateBlock();
-  await generateBlock();
-  await fs.writeFileSync(
-    './blockChain.json',
-    new Util().jsonSave(KOYN.chain),
-    'utf-8'
-  );
-  console.log('valid ->');
-  console.log(KOYN.checkValid());
-}
-
-mainProcess();
-
-
-
-
-
+new BlockChain().mainProcess({
+  rewardAddress: "APOJA7S8ASNA9S8WE",
+  totalBlocks: 3,
+  paths: [
+    {
+      url: 'http://localhost:3001/koyn',
+      type: 'App'
+    },
+    {
+      url: 'http://localhost:3001/cards',
+      type: 'App'
+    },
+    {
+      url: 'http://localhost:91/transactions',
+      type: 'Transaction'
+    }
+  ]
+});
 
 
 
